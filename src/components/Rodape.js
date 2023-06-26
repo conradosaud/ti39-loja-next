cf63db1d073e26859498d3e92cf6548b44d6e99a
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2023-06-26 17:24:23 -0300

    Rotas do menu e página de login

---
+++ src/components/Rodape.js
@@ -0,0 +1,11 @@
+export default function Rodape(){
+
+    // style={{position:"fixed", bottom:0}}
+
+    return(
+        <footer className="fixed bottom-0" >
+            <hr/>
+            <p> Conradito Store </p>
+        </footer>
+    )
+}
