a6fe4cf05ba1c7669cc7502007f1e9b29201c69d
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2023-06-27 17:19:31 -0300

    home completo e pagina de produtos iniciado

---
+++ src/app/carrinho/page.js
@@ -0,0 +1,15 @@
+
+export default function Carrinho(){
+    return(
+        <div>
+            <h1> Carrinho </h1>
+
+            <p> Quantidade de produtos: <strong>0</strong> </p>
+            <p> Total do carrinho: R$ <strong>0</strong> </p>
+
+            <hr/>
+
+            <p> Seu carrinho ainda está vazio... </p>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,39 @@
+'use client'
+import { useState, useEffect } from 'react';
 
+import { buscaTodos } from '@/model/produtos';
 
 export default function Home() {
+
+    const [ produtos, alteraProdutos ] = useState([])
+
+    useEffect( ()=> {
+        async function dados(){
+            const resposta = await buscaTodos();
+            alteraProdutos( resposta.data )
+        }
+        dados();
+    }, [] )
+
     return (
         <div>
-            <h1> Olá, mundo! </h1>
+            <h1> Conradito Store </h1>
+            <p> Confira nossos produtos: </p>
+
+            {
+                produtos == 0 ? <p> Carregando... </p> :
+                produtos.map( produto => 
+                    <div key={ produto.id } >
+                        <p> { produto.nome } </p>
+                        <p> { produto.preco } </p>
+                        <img src={ produto.imagem } />
+                        <hr/>
+                    </div>
+                )
+                
+                
+            }
+
         </div>
     )
 }
